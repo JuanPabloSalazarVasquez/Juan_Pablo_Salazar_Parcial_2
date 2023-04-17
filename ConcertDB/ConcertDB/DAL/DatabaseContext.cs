929a419ae282fe1d43a79145b027521e97765a8d
Juan Salazar <juanpasinga@gmail.com>
2023-04-17 16:37:37 -0500

    FirstAndFinalCommit

---
+++ ConcertDB/ConcertDB/DAL/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ConcertDB.DAL.Entities;
+using System.Diagnostics.Metrics;
+
+namespace ConcertDB.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(c => c.Id).IsUnique();
+        }
+    }
+}
---
+++ ConcertDB/ConcertDB/DAL/Entities/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConcertDB.DAL.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        public DateTime? UseDate { get; set; }
+        public bool IsUsed { get; set; }
+        public string? EntranceGate { get; set; }
+
+    }
+}
